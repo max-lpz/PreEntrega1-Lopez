199e4e324338c43992d259636e58044e348113ab
Maximo Nahuel Lopez <maximonahuellopez@gmail.com>
2023-06-05 23:25:54 -0300

    update while

--- js/login_carrito.js
+++ js/login_carrito.js
@@ -16,16 +16,8 @@ let prod4 = 0
 let user = prompt('Ingrese su usuario y precione aceptar')
 let pass = prompt('Ingrese su contraseña y precione aceptar')
 
-if ((user === "admin") && (pass === "pintura2023")) {
-    alert('Login ok, user: ' + user + "\nprecione aceptar para seleccionar sus productos")
-
-function seleccion() {
-    let prod = prompt('Ingrese la opción para agregar al carrito segun corresponda: \n# 1 - Pinturas nacionales\n# 2 - Pinturas importadas\n# 3 - Pinceles\n# 4 - Accesorios\n# 0 - Cerra compra')
-    switch (prod) {
-        case "":
-            alert("Complete con una opción")
-            if (prod = null) { }
-            break;
+function seleccion(opc) {
+    switch (opc) {
         case "0":
             alert("Finalizo")
             break;
@@ -46,21 +38,27 @@ function seleccion() {
             alert(`Agregaste al carrito: ${prod4} Accesorio/s`)
             break;
         default:
-            alert(`La opcion ${prod} no es una opción valida`)
+            alert(`La opcion ${opc} no es una opción valida`)
             break;
     }
-    return prod
 }
 
-while (seleccion() != 0) { }
+if ((user === "admin") && (pass === "pintura2023")) {
+    alert('Login ok, user: ' + user + "\nprecione aceptar para seleccionar sus productos")
 
-let total = prod1 + prod2 + prod3 + prod4
+    let prod = prompt('Ingrese la opción para agregar al carrito segun corresponda: \n# 1 - Pinturas nacionales\n# 2 - Pinturas importadas\n# 3 - Pinceles\n# 4 - Accesorios\n# 0 - Cerra compra')
+    while (prod != 0) {
+        seleccion(prod)
+        prod = prompt('Ingrese la opción para agregar al carrito segun corresponda: \n# 1 - Pinturas nacionales\n# 2 - Pinturas importadas\n# 3 - Pinceles\n# 4 - Accesorios\n# 0 - Cerra compra')
+    }
 
-if (total != 0) {
-    alert("Cantidad total de productos: " + total)
-    alert(`Cantidad de Pinturas nacionales: ${prod1}\nCantidad de Pinturas importadas: ${prod2}\nCantidad de Pinceles: ${prod3}\nCantidad de Accesorios: ${prod4}`)
-    alert("gracias por su compra!")
-}
+    let total = prod1 + prod2 + prod3 + prod4
+
+    if (total != 0) {
+        alert("Cantidad total de productos: " + total)
+        alert(`Cantidad de Pinturas nacionales: ${prod1}\nCantidad de Pinturas importadas: ${prod2}\nCantidad de Pinceles: ${prod3}\nCantidad de Accesorios: ${prod4}`)
+        alert("gracias por su compra!")
+    }
 
 } else if ((user !== "admin") && (pass === "pintura2023")) {
     alert('Error el usuario ingresado es incorrecto');
